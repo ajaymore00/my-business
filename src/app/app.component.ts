d047cd7f44c8dc462167469dcddafd1962e6f822
ajaymore00 <moreajay4398@gmail.com>
2025-09-20 21:46:11 +0530

    Batch Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
 import {FooterComponent} from "./components/footer/footer.component";
 import {MaterialModule} from "./material.module";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,MaterialModule,BrowserAnimationsModule, RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [CommonModule,MaterialModule, RouterOutlet,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
