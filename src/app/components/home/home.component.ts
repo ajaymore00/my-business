475fe16401e79dcb055d24ffe19f8d4405737ccb
ajaymore00 <moreajay4398@gmail.com>
2025-09-21 01:57:12 +0530

    Batch Changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
 interface Service {
   title: string;
   description: string;
   image: string;
 }
+interface Testimonial {
+  name: string;
+  feedback: string;
+}
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   businessName = 'Sanskruti Enterprises';
   tagline = 'Making Your Garden Green & Clean';
-contact = {
+
+  aboutText = `We are passionate about transforming outdoor spaces into green, fresh, and peaceful
+  areas where you can relax and enjoy nature. With years of experience in lawn care, gardening,
+  and tree maintenance, we provide reliable and professional services that keep your garden
+  beautiful throughout the year.`;
+
+
+  contact = {
     phone: '+91 839 085 2369',
     email: 'contact@sanskrutienterprises.com',
-    address: 'House No 110 B,Zadani,Vavoshi,Tal-Khalapur, Dist-Raigad Maharashtra 410203'
+    address: 'House No 110 B,Zadani,Vavoshi,Tal-Khalapur, Dist-Raigad Maharashtra 410203',
+     hours: 'Mon - Sun: 8:00 AM - 6:00 PM',
+      whatsappLink: 'https://wa.me/918390852369'
   };
   services: Service[] = [
     {
@@ -40,6 +54,17 @@ contact = {
       title: 'Plant Care',
       description: 'From watering to fertilizing, we ensure your plants thrive every season.',
       image: 'assets/plantCare.jpg'
+    }
+  ];
+
+    testimonials: Testimonial[] = [
+    {
+      name: 'Ravi Kulkarni',
+      feedback: 'They turned my messy backyard into a beautiful lawn. Very professional work!'
+    },
+    {
+      name: 'Priya Sharma',
+      feedback: 'Excellent gardening service, friendly team, and affordable prices. Highly recommend!'
     }
   ];
 }
